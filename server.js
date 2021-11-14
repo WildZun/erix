a7b22765de803a255f8f0c15c90b17c8c0e2cd92
WildZun <50769967+WildZun@users.noreply.github.com>
2021-11-14 18:23:46 +0100

    Update server.js

--- server.js
+++ server.js
@@ -34,4 +34,4 @@ fs.readdir("./events/", (err, files) => {
   });
 });
 
-bot.login("ODM0MDc0NDczNjkwNTYyNTcw.YH7mYg.SieBTsCuYTn-v2Jj1faqvELIqc4")
+bot.login("ODM0MDc0NDczNjkwNTYyNTcw.YH7mYg.PHYSQEv442tYBQmtQZF742yi3KE")
