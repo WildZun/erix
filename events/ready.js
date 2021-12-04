77ba39019c539a08158a58a8f308e1b2b4928729
Votre Nom <enzodufour59@gmail.com>
2021-12-04 22:59:36 +0100

    Création de la branche V2

--- events/ready.js
+++
@@ -1,8 +0,0 @@
-let db = require("quick.db")
-const fs =  require("fs")
-
-
-exports.run = async (bot) => {
-    bot.user.setPresence({ activity: { name: `vos serveurs ! e.help - ${bot.guilds.cache.size} serveurs !`, type: 3} })
-    console.log("✅ Je suis prêt !")
-    }
